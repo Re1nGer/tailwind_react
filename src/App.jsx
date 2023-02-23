e9d1870bb6b0958223d44e98b7003c7e3daeebda
Bekjon Ibragimov <bekjon_98@mail.ru>
2023-02-24 01:19:28 +0200

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-center text-red-500">Hello world!</h1> 
+    <Header />
   )
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+
+function Layout() {
+    return (
+    <section className="layout">
+
+    </section> );
+}
+
+export default Layout;
